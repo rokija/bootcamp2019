90536f9a019eb8e649e557ce22bd841b1f21ed75
Ruta <rutafreimane14@gmail.com>
2019-02-04 10:03:05 +0200

    created Posts component and Card body

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Posts from "./components/Posts";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Posts />
+      </div>
+    );
+  }
+}
+
+export default App;

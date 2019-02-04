beacc36f6b2a02d300db9f4328ac0dce6bd1dc52
Ruta <rutafreimane14@gmail.com>
2019-02-04 14:22:53 +0200

    created button for adding a post

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,21 @@
 import React from "react";
+import logo from "../../media/image-placeholder.svg";
 import "./Header.css";
 
-const Header = () => <div className="Header">Header</div>;
+const Header = () => (
+  <div className="Header">
+    <div className="row">
+      <div className="col-md-2">
+        <img className="Header__image" src={logo} alt="something" />
+        <span className="pull-right"> headline </span>
+      </div>
+      <div className="col-md-10">
+        <div className="Header__button">
+          <button className="btn btn-primary">create post</button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Header;

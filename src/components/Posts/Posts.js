faef0eb6ec9d9e5f0a87b1f99b13e020ca71946c
Ruta <rutafreimane14@gmail.com>
2019-02-04 10:13:07 +0200

    create many posts

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -3,18 +3,48 @@ import placeholderImage from "../../media/image-placeholder.svg"
 import "./Posts.css";
 
 const Posts = () => (
-    <div className="col-6">
-        <div className="card">
-            <img className="card-img-top" alt="some img" src={placeholderImage}/>
-            <div className="card-body">
-                <h2 className="card-title">
-                    Title
-                </h2>
-                <span className="card-text">
-                    some text
-                </span>
+    <div className="container">
+        <div className="row">
+            <div className="col-4">
+                <div className="card">
+                    <img className="card-img-top" alt="some img" src={placeholderImage}/>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Title
+                        </h2>
+                        <span className="card-text">
+                            some text
+                        </span>
+                    </div>
+                </div>        
             </div>
-        </div>        
+            <div className="col-4">
+                <div className="card">
+                    <img className="card-img-top" alt="some img" src={placeholderImage}/>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Title
+                        </h2>
+                        <span className="card-text">
+                            some text
+                        </span>
+                    </div>
+                </div>        
+            </div>
+            <div className="col-4">
+                <div className="card">
+                    <img className="card-img-top" alt="some img" src={placeholderImage}/>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Title
+                        </h2>
+                        <span className="card-text">
+                            some text
+                        </span>
+                    </div>
+                </div>        
+            </div>
+        </div>
     </div>
 )
 

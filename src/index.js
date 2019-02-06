fdcd00909ef0f08c4ed9c9cfeb28beb46ef9f1cd
Ruta <rutafreimane14@gmail.com>
2019-02-06 14:26:23 +0200

    removed react router

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
-import Login from "./components/Login";
-import Register from "./components/Register";
 import * as serviceWorker from "./serviceWorker";
 
-const Root = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-        </Switch>
-    </Router>
-)
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
